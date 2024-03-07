fc34526898a1252de7bb3a8ac8ef443a2a5e77ab
lyinggame4356 <lying.game4356@tutanota.com>
2024-03-07 14:09:38 +0200

    add new icons

---
+++ Distance.php
@@ -0,0 +1,82 @@
+<?php
+namespace Beer;
+
+class Distance
+{
+    /**
+     * Get distance between two coordinates
+     *
+     * @return float
+     * @param  float $latitude1
+     * @param  float $longitude1
+     * @param  float $latitude2
+     * @param  float $longitude2
+     * @param  int     $decimals[optional] The amount of decimals
+     * @param  string  $unit[optional]
+     */
+    public static function between(
+        $latitude1,
+        $longitude1,
+        $latitude2,
+        $longitude2,
+        $decimals = 1,
+        $unit = 'km'
+    ) {
+        // define calculation variables
+        $theta = $longitude1 - $longitude2;
+        $distance = (sin(deg2rad($latitude1)) * sin(deg2rad($latitude2)))
+            + (cos(deg2rad($latitude1)) * cos(deg2rad($latitude2)) * cos(deg2rad($theta)))
+        ;
+        $distance = acos($distance);
+        $distance = rad2deg($distance);
+        $distance = $distance * 60 * 1.1515;
+        // unit is km
+        if ($unit == 'km') {
+            // redefine distance
+            $distance = $distance * 1.609344;
+        }
+        // return with one decimal
+        return round($distance, $decimals);
+    }
+    /**
+     * Get closest location from all locations
+     *
+     * @return array   The item which is the closest + 'distance' to it.
+     * @param  float $latitude1
+     * @param  float $longitude1
+     * @param  array $items = array(array('latitude' => 'x', 'longitude' => 'x'), array(xxx))
+     * @param  int $decimals [optional] The amount of decimals
+     * @param  string $unit [optional]
+     */
+    public static function getClosest(
+        $latitude1,
+        $longitude1,
+        $items,
+        $decimals = 1,
+        $unit = 'km'
+    ) {
+        // init result
+        $distances = array();
+        // loop items
+        foreach ($items as $key => $item) {
+            // define second item
+            $latitude2 = $item['latitude'];
+            $longitude2 = $item['longitude'];
+            // define distance
+            $distance = self::between(
+                $latitude1,
+                $longitude1,
+                $latitude2,
+                $longitude2,
+                10,
+                $unit
+            );
+            // add distance
+            $distances[$distance] = $key;
+            // add rounded distance to array
+            $items[$key]['distance'] = round($distance, $decimals);
+        }
+        // return the item with the closest distance
+        return $items[$distances[min(array_diff(array_map('floatval',array_keys($distances)), array(0)))]];
+    }
+}
